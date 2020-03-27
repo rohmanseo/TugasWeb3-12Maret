83a85c21464bec09eda99c1264fd2e79610a9dae
rohman <rohmann311@gmail.com>
2020-03-27 18:57:56 +0700

    Updates

---
+++ precache-manifest.123788f1231090ca3dbfa042f9acca3d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bff9dfd45c6097e524f6fea37d9e6953",
+    "url": "/WEB3A-431737-NUR-ROHMAN-REACT/index.html"
+  },
+  {
+    "revision": "223e2c99062cfef0d4ac",
+    "url": "/WEB3A-431737-NUR-ROHMAN-REACT/static/css/2.1bd2ab2f.chunk.css"
+  },
+  {
+    "revision": "c1456eca84b2cbe3c39e",
+    "url": "/WEB3A-431737-NUR-ROHMAN-REACT/static/css/main.35a29039.chunk.css"
+  },
+  {
+    "revision": "223e2c99062cfef0d4ac",
+    "url": "/WEB3A-431737-NUR-ROHMAN-REACT/static/js/2.3ab9cfeb.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/WEB3A-431737-NUR-ROHMAN-REACT/static/js/2.3ab9cfeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1456eca84b2cbe3c39e",
+    "url": "/WEB3A-431737-NUR-ROHMAN-REACT/static/js/main.44bc65d6.chunk.js"
+  },
+  {
+    "revision": "d0c9f4ff36945eb522d3",
+    "url": "/WEB3A-431737-NUR-ROHMAN-REACT/static/js/runtime-main.f610d7ce.js"
+  },
+  {
+    "revision": "976bdfb28b207897b789377a8292f9b6",
+    "url": "/WEB3A-431737-NUR-ROHMAN-REACT/static/media/intro2.976bdfb2.jpg"
+  }
+]);
